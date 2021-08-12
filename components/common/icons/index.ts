aaaac2acae97105064db7ef39dcbd205e5ef97f8
Dolosolow <Jose.Munoz07c@gmail.com>
2021-08-12 11:56:52 -0400

    Added icons to the icons folder that are to be used in the services components.

--- components/common/icons/index.ts
+++ components/common/icons/index.ts
@@ -1,5 +1,9 @@
+export { CeilFanIcon } from "./CeilFanIcon";
 export { CheckIcon } from "./CheckIcon";
+export { DigIcon } from "./DigIcon";
 export { ElectricIcon } from "./ElectricIcon";
+export { FloorTileIcon } from "./FloorTileIcon";
+export { GarageIcon } from "./GarageIcon";
 export { HandShakeIcon } from "./HandShakeIcon";
 export { HomeRepairIcon } from "./HomeRepairIcon";
 export { PuzzleIcon } from "./PuzzleIcon";
