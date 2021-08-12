d113cf47f0d1971938b42580b5b68f9c20685d67
Dolosolow <Jose.Munoz07c@gmail.com>
2021-08-12 12:16:03 -0400

    Added text content for services page components.

---
+++ lang/en-services.ts
@@ -0,0 +1,65 @@
+import type { ServicesContent } from "./lang";
+
+export const content: ServicesContent = {
+  headline: {
+    descriptor: "Our Services",
+    colorTitle: "Harbor",
+    title: "Home Services",
+    caption: `Behind every successful business, there is a group of talented people, committed to success.`,
+  },
+  description: `Here at Harbor Home Services, since opening in 2018 our primary goal in all business
+  decisions is to achieve a high level of customer satisfaction with all that we provide.
+  Which has fueled our growth and will continue to do so for years down the road. Browse our
+  site to discover what we're all about.`,
+  cards: [
+    {
+      title: "Assemble Furniture",
+      content: "New appliance/furniture to install? Get professional appliance help fast.",
+    },
+    {
+      title: "Electrical",
+      content: "New appliance/furniture to install? Get professional appliance help fast.",
+    },
+    {
+      title: "Fan Installation",
+      content: "New appliance/furniture to install? Get professional appliance help fast.",
+    },
+    {
+      title: "Floors",
+      content: "New appliance/furniture to install? Get professional appliance help fast.",
+    },
+    {
+      title: "Garage",
+      content: "New appliance/furniture to install? Get professional appliance help fast.",
+    },
+    {
+      title: "Handyman",
+      content: "New appliance/furniture to install? Get professional appliance help fast.",
+    },
+    {
+      title: "Landscaping",
+      content: "New appliance/furniture to install? Get professional appliance help fast.",
+    },
+    {
+      title: "Painting",
+      content: "New appliance/furniture to install? Get professional appliance help fast.",
+    },
+    {
+      title: "Property Maintenance",
+      content: "New appliance/furniture to install? Get professional appliance help fast.",
+    },
+  ],
+  otherServices: [
+    "Shelving & storage",
+    "Bedrooms",
+    "Drywalls",
+    "Remodeling",
+    "Kitchens",
+    "Handyman",
+    "Garden sheds",
+    "Gazebos",
+    "Ceiling & roofing",
+    "Loft conversion",
+    "Gates and fences",
+  ],
+};
