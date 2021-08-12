60f71c1ee167a2ec9f01f146ee2c350181c4f674
Dolosolow <Jose.Munoz07c@gmail.com>
2021-08-12 08:32:22 -0400

    Created About page file and added the text content for the page under en-about.ts.

---
+++ lang/en-about.ts
@@ -0,0 +1,30 @@
+import type { AboutContent } from "./lang";
+
+export const content: AboutContent = {
+  headline: {
+    descriptor: "Our Services",
+    colorTitle: "Harbor",
+    title: "Home Services",
+    caption: `Behind every successful business, there is a group of talented people, committed to success.`,
+  },
+  description: `Here at Harbor Home Services, since opening in 2018 our primary goal in all business
+    decisions is to achieve a high level of customer satisfaction with all that we provide.
+    Which has fueled our growth and will continue to do so for years down the road. Browse our
+    site to discover what we're all about.`,
+  cards: [
+    {
+      title: "Personalized Approach",
+      content: `We are delighted to provide the best services so we work on your schedule and to your
+            request while delivering our top quality work.`,
+    },
+    {
+      title: "Professionalism",
+      content: `Focused on providing exceptional service, we have built outstanding relationships with
+        our customers.`,
+    },
+    {
+      title: "Licensed and Insured",
+      content: `License# 13VH11535600:Policy# UDC-4827137-CGL`,
+    },
+  ],
+};
---
+++ pages/about/index.tsx
@@ -0,0 +1,13 @@
+import { LayoutWithPosters } from "@/components/common/layout";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+};
+
+export default About;
+
+About.getLayout = LayoutWithPosters;
