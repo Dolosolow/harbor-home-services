0b148552128636f4532d4cbc023398949ed1ac9b
Dolosolow <Jose.Munoz07c@gmail.com>
2021-08-16 18:37:19 -0400

    Created other half of the contact page, ContactLocation, image container to show address on a map.

---
+++ components/pages/contact/ContactLocation.tsx
@@ -0,0 +1,56 @@
+import { Flex, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
+import { ChakraNextImage } from "@/components/common/chakra-next-image";
+import { content } from "@/lang/en-home";
+
+export const ContactLocation = () => {
+  const { html } = content;
+  const [isHovered, setIsHovered] = useState(false);
+
+  const onHoverEnter = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setIsHovered(true);
+  };
+
+  const onHoverExit = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setIsHovered(false);
+  };
+
+  return (
+    <Flex
+      h="100%"
+      w="100%"
+      position="relative"
+      onMouseEnter={onHoverEnter}
+      onMouseLeave={onHoverExit}
+    >
+      <Flex
+        bgColor="#121212b2"
+        border="1px solid #ffffff67"
+        borderRadius={8}
+        fontSize={14}
+        flexDir="column"
+        position="absolute"
+        p={4}
+        top={[14, null, 20, 500, 450]}
+        left={[200, 160, 280, 140, 200]}
+        zIndex={10}
+        opacity={isHovered ? 1 : 0}
+        transform={isHovered ? "scale(1)" : "scale(0)"}
+        transformOrigin="top left"
+        transition="all 0.4s ease-in-out"
+      >
+        <Text color="white">{html.meta.phone}</Text>
+        <Text color="white">{html.meta.address}</Text>
+        <Text color="white">{html.meta.city}</Text>
+      </Flex>
+      <ChakraNextImage
+        priority
+        src="https://res.cloudinary.com/dnrj5jpxf/image/upload/v1641681152/hhs-images/loc_yeshte.png"
+        h="100%"
+        w="100%"
+        objectFit="cover"
+      />
+    </Flex>
+  );
+};
---
+++ pages/contact/index.tsx
@@ -0,0 +1,46 @@
+import { Flex } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
+
+import { getAllServices } from "@/utils/getAllServices";
+import type { CFCProps } from "@/components/pages/contact/ContactForm";
+
+const DynamicContactForm = dynamic<CFCProps>(() =>
+  import("../../components/pages/contact/ContactForm").then((mod) => mod.ContactForm)
+);
+const DynamicLocation = dynamic<{}>(() =>
+  import("../../components/pages/contact/ContactLocation").then((mod) => mod.ContactLocation)
+);
+
+const services = getAllServices();
+
+const Contact = () => {
+  const onFormSubmit = () => {
+    window.scrollTo({ behavior: "smooth", top: 0, left: 0 });
+  };
+
+  return (
+    <div className="container container-contact">
+      <Flex
+        bgColor="white.700"
+        align="center"
+        justify="center"
+        pb={20}
+        flex={1}
+        w={["100%", null, null, "50%"]}
+        order={[2, null, null, 1]}
+      >
+        <DynamicContactForm services={services} onFormSubmit={onFormSubmit} />
+      </Flex>
+      <Flex
+        w="100%"
+        flex={["auto", null, null, 1]}
+        h={["200px", null, "300px", "1222px"]}
+        order={[1, null, null, 2]}
+      >
+        <DynamicLocation />
+      </Flex>
+    </div>
+  );
+};
+
+export default Contact;
