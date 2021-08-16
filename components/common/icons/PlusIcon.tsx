2541e1fb6ca2aac93f2e0da774b62e4f95e9e58b
Dolosolow <Jose.Munoz07c@gmail.com>
2021-08-16 09:28:42 -0400

    Added icons to icon folder. Updated folder's index file.

---
+++ components/common/icons/PlusIcon.tsx
@@ -0,0 +1,16 @@
+import { createIcon } from "@chakra-ui/react";
+
+export const PlusIcon = createIcon({
+  displayName: "PlusIcon",
+  viewBox: "0 45 512 512",
+  path: (
+    <path
+      d="M243 48v186.5H62.5v51H243v184h54.5v-184H480v-51H297.5V48H243Z"
+      fill="currentColor"
+      stroke="currentColor"
+      strokeWidth="16"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  ),
+});
--- components/common/icons/index.ts
+++ components/common/icons/index.ts
@@ -7,6 +7,7 @@ export { FloorTileIcon } from "./FloorTileIcon";
 export { GarageIcon } from "./GarageIcon";
 export { HandShakeIcon } from "./HandShakeIcon";
 export { HomeRepairIcon } from "./HomeRepairIcon";
+export { PlusIcon } from "./PlusIcon";
 export { PuzzleIcon } from "./PuzzleIcon";
 export { Rollerbrush } from "./RollerbrushIcon";
 export { ShieldIcon } from "./ShieldIcon";
