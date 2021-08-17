8bdc07620cb229b674d4dec2865002208bd92b27
Dolosolow <Jose.Munoz07c@gmail.com>
2021-08-17 10:12:44 -0400

    Created a services folder in the project to contain route logic. Added module alias for it on the tsconfig.json. Modified the api as needed.

--- pages/api/email/index.ts
+++ pages/api/email/index.ts
@@ -1,10 +1,5 @@
-import { ValidationError } from "yup";
-import type { FormikErrors } from "formik";
-
 import { configureNextApiRoutes } from "@/utils/api/configureNextApi";
-import { inquirySchema } from "@/utils/validation-schemas";
-import SGMailProvider from "@/utils/api/SGMailProvider";
-import type { FormValues } from "../../../global";
+import { processEmailRequest } from "@/services/processEmailRequest";
 
 const ApiRoute = configureNextApiRoutes();
 
@@ -12,57 +7,7 @@ ApiRoute.get((req, res) => {
   res.send("This is the Email API routes");
 });
 
-ApiRoute.post(async (req, res) => {
-  console.log("email api request received\n");
-  const sgMail = new SGMailProvider();
-  let attachments: Array<any> = [];
-
-  const formValues = req.body.formValues;
-  const formFiles = req.files;
-
-  attachments = formFiles.map((file) => {
-    return {
-      content: file.buffer.toString("base64"),
-      filename: file.originalname,
-      type: "image/jpeg",
-      diposition: "attachment",
-    };
-  });
-
-  try {
-    const validatedData = await inquirySchema.validate(formValues, { abortEarly: false });
-    const newInquiry = Object.assign(validatedData, {
-      phone: validatedData.phone ?? "–––––––––––––",
-      selectedServices: validatedData.selectedServices.join(", "),
-    });
-
-    delete newInquiry["project_images"];
-
-    await sgMail.sendMail({
-      from: process.env.CLIENT_SENDER_EMAIL as string,
-      to: newInquiry.email,
-      templateId: process.env.SENDGRID_EMAIL_TEMPLATE_ID as string,
-      dynamicTemplateData: newInquiry,
-      attachments,
-    });
-
-    return res.status(200).send(`email has successfully been sent`);
-  } catch (err) {
-    if (err instanceof ValidationError) {
-      let error: FormikErrors<FormValues> = {};
-
-      err.inner.map((e) => {
-        let formattedError: { [key: string]: string } = {};
-        formattedError[e.path!] = e.errors[0];
-        error = { ...formattedError };
-      });
-
-      return res.status(400).send(error);
-    } else {
-      return res.status(401).send(`email has failed to be sent`);
-    }
-  }
-});
+ApiRoute.post(processEmailRequest);
 
 export default ApiRoute;
 
