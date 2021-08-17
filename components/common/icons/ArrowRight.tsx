cd46f593e25d2c5ced01cf35407da83c7340099a
Dolosolow <Jose.Munoz07c@gmail.com>
2021-08-16 21:09:10 -0400

    Added icons to icon folder. Updated folder's index file.

---
+++ components/common/icons/ArrowRight.tsx
@@ -0,0 +1,14 @@
+import { createIcon } from "@chakra-ui/react";
+
+export const ArrowRight = createIcon({
+  displayName: "ArrowRight",
+  viewBox: "0 -55 512 512",
+  path: (
+    <path
+      d="M15 176h480m0 0L349.152 15M495 176 349.152 337"
+      stroke="currentColor"
+      strokeWidth="30"
+      strokeLinecap="round"
+    />
+  ),
+});
---
+++ components/common/icons/ClosedCircleIcon.tsx
@@ -0,0 +1,16 @@
+import { createIcon } from "@chakra-ui/react";
+
+export const CloseCircleIcon = createIcon({
+  displayName: "CloseIcon",
+  viewBox: "0 0 512 512",
+  path: (
+    <path
+      d="M185.5 183.5 327 325m-141 .5L327.5 184m-72-165c142.5 11.5 236 98 236 236 6.5 98-86 242.5-236 236-116.5 0-227-79-236-236-5.0159-87.5 69.6-231.6 236-236Z"
+      fill="transparent"
+      stroke="currentColor"
+      strokeWidth="38"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  ),
+});
